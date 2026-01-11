76e16c2f571fe1a1d488209d7635dfe2594acf80
Sam Bulatov <mephistorine@gmail.com>
2026-01-11 18:18:17 +0300

    chore: add .env support in app

--- apps/kastle/plugins/env-var-plugin.js
+++ apps/kastle/plugins/env-var-plugin.js
@@ -1,18 +1,17 @@
-const myOrgEnvRegex = /^KSTL_/i;
-
 const envVarPlugin = {
     name: "env-var-plugin",
     setup(build) {
         const options = build.initialOptions;
 
-        const envVars = {};
         for (const key in process.env) {
-            if (myOrgEnvRegex.test(key)) {
-                envVars[key.replace(myOrgEnvRegex, "")] = process.env[key];
+            if (key.startsWith("KSTL_")) {
+                Reflect.set(
+                    options.define,
+                    `import.meta.env.${key.replace("KSTL_", "")}`,
+                    JSON.stringify(process.env[key]),
+                );
             }
         }
-
-        options.define["import.meta.env"] = JSON.stringify(envVars);
     },
 };
 
